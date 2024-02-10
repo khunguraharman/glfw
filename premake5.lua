917f76c1c5212597ce2447a1290054a091914894
khunguraharman <khungura_harman@hotmail.com>
2024-02-10 12:22:32 -0800

    Corrected Typo in Premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -2,10 +2,11 @@ project "GLFW"
 	kind "StaticLib"
 	language "C"
 
-		targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-		objdir ("obj/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("obj/" .. outputdir .. "/%{prj.name}")
 
-		files{
+	files
+	{
 		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
 		"src/glfw_config.h",
@@ -14,29 +15,31 @@ project "GLFW"
 		"src/monitor.c",
 		"src/vulkan.c",
 		"src/window.c"
-		}
+	}
 
-		filter "system:windows"
-		buildoptions {"-std=c11", "lgdi32"}
+	filter "system:windows"
+		buildoptions {"-std=c11", "-lgdi32"}
 		systemversion "latest"
 		staticruntime "On"
 
-		files{
-		"src/win32_init.c",
-		"src/win32_joystick.c",
-		"src/win32_monitor.c",
-		"src/win32_time.c",
-		"src/win32_thread.c",
-		"src/win32_window.c",
-		"src/wgl_context.c",
-		"src/egl_context.c",
-		"src/osmesa_context.c"
+		files
+		{
+			"src/win32_init.c",
+			"src/win32_joystick.c",
+			"src/win32_monitor.c",
+			"src/win32_time.c",
+			"src/win32_thread.c",
+			"src/win32_window.c",
+			"src/wgl_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c"
 		}
 
-		defines{
-		"_GLFW_WIN32",
-		"_CRT_SECURE_NO_WARNINGS"
+		defines
+		{
+			"_GLFW_WIN32",
+			"_CRT_SECURE_NO_WARNINGS"
 		}
 
-		filter { "system:windows","configurations:Release"}
-		buildopptions "/MT"
+	filter { "system:windows","configurations:Release"}
+		buildoptions "/MT"
